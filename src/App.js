a1ef13baf0384df0739f7400d39964d188ad411d
angelopoole <angelo.poole@outlook.com>
2020-11-24 15:39:42 -0500

    added burger ingredient css, burger ingredient class

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 
+//components
 import Layout from './components/Layout/Layout';
+
+// containers
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 export default class App extends Component {
 	render() {
 		return (
 			<div>
 				<Layout>
-					<p> Test </p>
+					<BurgerBuilder />
+					{console.log({ hit: 'hit' })}
 				</Layout>
 			</div>
 		);
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,11 @@
 // before react fragments we would wrap the lower components with an <Aux> tag in order to have these tags displayed next to eachother.
 
+import classes from './Layout.module.css';
+
 const layout = props => (
 	<>
 		<div>Toolbar, SideDrawer, BackDrop</div>
-		<main>{props.children}</main>
+		<main className={classes.Content}>{props.children}</main>
 	</>
 );
 
