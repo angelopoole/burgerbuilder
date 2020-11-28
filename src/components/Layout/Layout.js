b82dbfbdae46e8149a2c2869da5984a27b8d0d5d
angelopoole <angelo.poole@outlook.com>
2020-11-28 12:47:39 -0500

    logo build, working on navigation itemd

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 // before react fragments we would wrap the lower components with an <Aux> tag in order to have these tags displayed next to eachother.
 
 import classes from './Layout.module.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const layout = props => (
 	<>
-		<div>Toolbar, SideDrawer, BackDrop</div>
+		<Toolbar />
 		<main className={classes.Content}>{props.children}</main>
 	</>
 );
