5f04b3794d3908cbafbc024cd16c3c56957ed94c
angelopoole <angelo.poole@outlook.com>
2020-12-06 23:47:11 -0500

    Added comment on hamburger component

--- src/components/Navigation/sideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/sideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,5 +1,7 @@
 import classes from './DrawerToggle.module.css';
 
+// Hamburger consists of 3 divs that are styled to look like little bars
+
 const drawerToggle = props => (
 	<div onClick={props.openSideBar} className={classes.DrawerToggle}>
 		<div></div>
